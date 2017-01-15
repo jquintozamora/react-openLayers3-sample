b02f18cb9a20fbb3c4638f80c4442c31e685afa8
Jose Quinto <quintorel@hotmail.com>
2017-01-15 23:16:09 +0000

    update polyfill loader

--- app/src/utils/polyfillDynamicLoader.js
+++ app/src/utils/polyfillDynamicLoader.js
@@ -17,10 +17,29 @@
 // Dynamic polyfill
 //   https://github.com/PascalAOMS/dynamic-polyfill
 
+// 
 
 // Support: 
 //  http://caniuse.com/#search=lastIndexOf - Not supported IE8
 
+
+// Good programming with featureString: https://github.com/Financial-Times/polyfill-service/blob/master/service/PolyfillSet.js
+
+
+function getDescendantProp(obj, desc) {
+  var arr = desc.split(".");
+  while (arr.length) {
+    var name = arr.shift();
+    //console.log('obj: ' + obj + ', name: ' + name);
+    if (name in obj) {
+      obj = obj[name];
+    } else {
+      console.warn('[browserSupportsAllFeatures] - ' +obj + ' has not ' + name + " property.");
+      return undefined;
+    }
+  }
+  return obj;
+}
 
 export function browserSupportsAllFeatures(featureString) {
 
@@ -29,19 +48,30 @@ export function browserSupportsAllFeatures(featureString) {
 
   var supportAllFeatures = true;
 
-  arrayFeatures.forEach( function (item) {
-      // Get the feature and the container Object
-      var feature = item.substring(item.lastIndexOf('.') + 1);
-      var object = "window";
-      var tempObject = item.substring(0,item.lastIndexOf('.'));
-      if ( tempObject !== "" ) {
-        object += "." + tempObject;
-      }
-      if(feature in eval(object) === false) {
+  arrayFeatures.forEach(function (item) {
+    // Get the feature and the container Object
+    var featureString = item.substring(item.lastIndexOf('.') + 1);
+    var objectString = item.substring(0, item.lastIndexOf('.'));
+
+    // Use property accessors intead using eval
+    //  eval: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval
+    //  property accessors: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_Accessors
+    // if(feature in eval(object) === false) {
+    //   supportAllFeatures = false;
+    //   return;
+    // }
+    var object = getDescendantProp(window, objectString);
+    if (object !== undefined) {
+      if (featureString in object === false) {
         supportAllFeatures = false;
         return;
       }
+    } else {
+      supportAllFeatures = false;
+      return;
+    }
   });
+
   return supportAllFeatures;
 }
 
